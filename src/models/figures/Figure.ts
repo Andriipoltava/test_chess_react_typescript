b5961f02b8c6174e20e04bae3c21bba1a4930e73
andriiomelianenko <omelianenko993@gmail.com>
2022-05-17 00:22:21 +0300

    Load app chess

---
+++ src/models/figures/Figure.ts
@@ -0,0 +1,46 @@
+import logo from "../../assets/black-bishop.png"
+import {Colors} from "../Colors";
+import {Cell} from "../Cell";
+
+export enum FigureNames {
+    FIGURE = 'FIGURE',
+    KING = 'KING',
+    KNIGHT = 'KNIGHT',
+    PAWN = 'PAWN',
+    QUEEN = 'QUEEN',
+    ROOK = 'ROOK',
+    BISHOP = 'BISHOP',
+}
+
+export class Figure {
+    color: Colors;
+    logo: typeof logo | null;
+    cell: Cell;
+    name: FigureNames;
+    id: number;
+
+    constructor(color: Colors, cell: Cell) {
+        this.color = color;
+        this.cell = cell;
+        this.cell.figure = this;
+        this.name = FigureNames.FIGURE;
+        this.id = Math.random();
+        this.logo = null;
+    }
+
+    canMove(target: Cell): boolean {
+        if (target.figure?.color === this.color) {
+            return false
+        }
+        if (target.figure?.name === FigureNames.KING) {
+            return false
+        }
+        return true
+
+    }
+
+    moveFigure(target: Cell): void {
+
+    }
+
+}
---
+++ src/models/figures/Pawn.ts
@@ -0,0 +1,41 @@
+import {Figure, FigureNames} from "./Figure";
+import {Colors} from "../Colors";
+import {Cell} from "../Cell";
+import whiteLogo from "../../assets/white-pawn.png";
+import blackLogo from "../../assets/black-pawn.png";
+
+export class Pawn extends Figure {
+    isFirstStep: boolean = true;
+
+    constructor(color: Colors, cell: Cell) {
+        super(color, cell);
+        this.logo = color === Colors.WHITE ? whiteLogo : blackLogo;
+        this.name = FigureNames.PAWN
+    }
+
+    canMove(target: Cell): boolean {
+        if (!super.canMove(target)) {
+            return false
+        }
+        const dir = this.cell.figure?.color === Colors.BLACK ? 1 : -1;
+        const firstStepDir = this.cell.figure?.color === Colors.BLACK ? 2 : -2;
+
+        // eslint-disable-next-line no-mixed-operators
+        if ((target.y === this.cell.y + dir || this.isFirstStep && (target.y === this.cell.y + firstStepDir))
+            && target.x === this.cell.x && this.cell.board.getCell(target.x, target.y).isEmpty()) {
+            return true
+        }
+        if (target.y === this.cell.y + dir && (target.x === this.cell.x + 1|| target.x === this.cell.x - 1)
+            &&  this.cell.isEnemy(target)) {
+            return true
+        }
+
+        return false
+    }
+
+    moveFigure(target: Cell) {
+        super.moveFigure(target);
+        this.isFirstStep = false
+    }
+
+}
